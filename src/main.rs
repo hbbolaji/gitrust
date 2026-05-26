9543a85074d79a2d0d279151631a06b88bdfde61
Hbbolaji <bolaji1050@gmail.com>
2026-05-26 08:02:36 +0100

    initialize git with env args and fs crate

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,14 @@
-use std::fs;
-
-use clap::{ Parser, Subcommand };
-
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-  #[arg(short, long)]
-  name: String,
-  #[arg(short, long, default_value_t = 1)]
-  count: u8,
-
-  #[command(subcommand)]
-  command: Command,
-}
-
-
-#[derive(Subcommand, Debug)]
-enum Command {
-    /// Doc comment
-    Init,
-}
+use std::{env::args, fs};
 
 fn main() {
-  let args = Args::parse();
-
-  match args.command {
-    Command::Init => {
-      fs::create_dir(".git").unwrap();
-      fs::create_dir(".git/objects").unwrap();
-      fs::create_dir(".git/refs").unwrap();
-      fs::write(".git/HEAD", "ref: refs/head/main\n").unwrap();
-      println!("Initialized git directory")
-    }
+  let args: Vec<String> = args().collect();
+  if args[1] == "init" {
+    fs::create_dir(".git").unwrap();
+    fs::create_dir("./git/objects").unwrap();
+    fs::create_dir(".git/refs").unwrap();
+    fs::write("./git/HEAD", "refs/head/main\n").unwrap();
+    println!("Initialized git directory");
+  } else {
+    println!("Unknow command: {}", args[1])
   }
 }
